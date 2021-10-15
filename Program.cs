b5112854eb03c1a9b1f479cb45ab01ce558b6874
Scott Truong <asdfgscott@gmail.com>
2021-10-15 18:34:59 -0400

    Finished

--- Program.cs
+++ Program.cs
@@ -17,10 +17,6 @@ namespace Login
             string password = Console.ReadLine();
             return password;
         }
-
-        static void login(){
-            
-        }
         static void Main(string[] args)
         {
             var users = new Dictionary<string, string>(){
@@ -33,8 +29,6 @@ namespace Login
             string user = username();
             string pass = password();
             int tryCount = 1;
-            // bool userCheck = false;
-            // bool passCheck = false;
 
             if(users.ContainsKey(user) && pass == users[user]){
                 Console.WriteLine("You're logged in");
@@ -52,62 +46,14 @@ namespace Login
                             user = username();
                             pass = password();
                             users.Add(user, pass);
+                            Console.WriteLine("You're registered and logged in!");
                         }
-                        else{
-                            tryCount = 1;
+                        else if(choice == "n"){
+                            tryCount = 0;
                         }
                     }
                 }
-                Console.WriteLine("You're logged in");
             }
-
-            // while(userCheck == false){
-            //     if(users.ContainsKey(user) == false){
-            //         Console.WriteLine("User not found");
-            //         user = username();
-            //     }
-            //     else{
-            //         userCheck = true;
-            //     }
-            // }
-
-            // while(passCheck == false){
-            //     if(pass != users[user]){
-            //         Console.WriteLine("Password is wrong");
-            //         pass = password();
-            //     }
-            //     else{
-            //         passCheck = true;
-            //     }
-            // }
-
-
-            // Console.WriteLine(userCheck);
-
-            // while(users.ContainsKey(username()) == false) {
-            //     Console.WriteLine("User not found");
-            //     user = username();
-            // }
-
-
-            // if(user == users[user]){
-            //     if(users.ContainsValue(pass) == false){
-            //         Console.WriteLine("Password is wrong");
-            //     }
-            //     else{
-            //         Console.WriteLine("You're logged in");
-            //     }
-            // }
-            //Check if the combo is in the dictionary
-            //If yes, check if the key matches the password entered 
-            //If no, say invalid login, prompt user to register or retry
-            //If register, add the user,pass to the dictionary
-
-            // users.Add(user,pass);
-
-            //Telling me that my inputs are being red
-            // Console.WriteLine("User name is: " + user);
-            // Console.WriteLine("Password is: " + users[user]);
         }
     }
 }
