89bb158207fadbefe263ef35abbbbfbb4aa4b3ca
Scott Truong <asdfgscott@gmail.com>
2021-10-15 17:06:00 -0400

    Started

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Login
 {
     class Program
     {
+        static void login(){
+                Console.WriteLine("--Starting Functing things--");
+                Console.WriteLine("Username: ");
+                string username = Console.ReadLine();
+                
+            }
+            static string username(){
+                Console.WriteLine("Username: ");
+                string username = Console.ReadLine();
+                return username;
+            }
+            static string password(){
+                Console.WriteLine("Password: ");
+                string password = Console.ReadLine();
+                return password;
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var users = new Dictionary<string, string>();
+            Console.WriteLine("Starting");
+
+            string user = username();
+            string pass = password();
+
+
+            Console.WriteLine("User name is: " + user);
+            Console.WriteLine("Password is: " + pass);
         }
     }
 }
