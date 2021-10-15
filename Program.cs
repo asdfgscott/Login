e883ea02b56896ea9cbcae7a9423d12b72c8af32
Scott Truong <asdfgscott@gmail.com>
2021-10-15 18:40:14 -0400

    Added comments

--- Program.cs
+++ Program.cs
@@ -7,11 +7,13 @@ namespace Login
 {
     class Program
     {
+        //Method to get a username
         static string username(){
             Console.WriteLine("Username: ");
             string username = Console.ReadLine();
             return username;
         }
+        //Method to get a password
         static string password(){
             Console.WriteLine("Password: ");
             string password = Console.ReadLine();
@@ -19,33 +21,42 @@ namespace Login
         }
         static void Main(string[] args)
         {
+            //Declaring the dictionary of existing users
             var users = new Dictionary<string, string>(){
                 {"user1", "pass1"},
                 {"user2", "pass2"},
                 {"user3", "pass3"}
             };
-            Console.WriteLine("Starting");
 
+            //Start here
+            //Begins by calling upon the username and password functions to get user input
             string user = username();
             string pass = password();
             int tryCount = 1;
 
+            //Checks if the user entered is in the dictionary
+            //Checks if the password entered is matching the value of key provided(user) 
             if(users.ContainsKey(user) && pass == users[user]){
                 Console.WriteLine("You're logged in");
             }
             else{
-                while((users.ContainsKey(user) && pass == users[user]) == false){
+                //While the username and password being entered is not in the dictionary, it'll run the loop
+                while((users.ContainsKey(user) && pass == users[user]) != true){
                     Console.WriteLine("Invalid login");
                     user = username();
                     pass = password();
                     tryCount++;
+                    //Too many attempts prompts the user if they want to register
                     if(tryCount == 3){
                         Console.WriteLine("Do you want to register? y/n");
                         string choice = Console.ReadLine();
                         if(choice == "y"){
                             user = username();
                             pass = password();
+                            //Adds the user information into the dictionary so the
+                            //condition on line 44 becomes true
                             users.Add(user, pass);
+                            //Since you registered, you technically logged in
                             Console.WriteLine("You're registered and logged in!");
                         }
                         else if(choice == "n"){
